768894546cd21a39f60003bb0453b2b8f35d9f10
Imac-marc <marc.arnoult@hotmail.fr>
2017-08-05 21:07:53 +0200

    Upgrade project for sensionlabs analyze

--- src/AppBundle/Action/HomeAction.php
+++ src/AppBundle/Action/HomeAction.php
@@ -6,23 +6,15 @@ use AppBundle\Responder\HomeResponder;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Templating\EngineInterface;
-use Symfony\Component\Translation\TranslatorInterface;
 
 class HomeAction
 {
-    private $twig;
-    private $translator;
 
-    public function __construct(
-        EngineInterface $twig,
-        TranslatorInterface $translator,
-        HomeResponder $responder
-    )
+    private $responder;
+
+    public function __construct(HomeResponder $responder)
     {
         $this->responder = $responder;
-        $this->translator = $translator;
-        $this->twig = $twig;
     }
 
     /**
