b26fd3d4dae11454b3f27f70c72efc0411fc2bdd
Imac-marc <marc.arnoult@hotmail.fr>
2017-08-19 15:59:20 +0200

    Rewrite fixtures

--- src/AppBundle/Domain/DataFixtures/ORM/LoadTicketsData.php
+++ src/AppBundle/Domain/DataFixtures/ORM/LoadTicketsData.php
@@ -19,26 +19,26 @@ class LoadTicketsData implements FixtureInterface
     public function load(ObjectManager $manager)
     {
         $command = new Command();
-        $ticket = new Ticket();
-
-        $ticket->setCreatedAt(new \DateTime('NOW'));
-        $ticket->setEntryAt(new \DateTime('2017-09-09'));
-        $ticket->setReduction(false);
-        $ticket->setLastName('Arnoult');
-        $ticket->setFirstName('Marc');
-        $ticket->setCountry('France');
-        $ticket->setBirthday(new \DateTime('1991-09-01'));
-        $ticket->setType("Journée");
-        $ticket->setPrice(16);
-
         $command->setEmail('marc.arnoult@hotmail.fr');
         $command->setPrice(16);
         $command->setCreatedAt(new \DateTime('NOW'));
-        $command->setType($ticket->getType());
 
-        $ticket->setCommand($command);
+        for ($i = 0; $i < 10; $i++) {
+            $ticket = new Ticket();
+            $ticket->setEntryAt(new \DateTime('2017-09-09'));
+            $ticket->setReduction(false);
+            $ticket->setCreatedAt(new \DateTime('NOW'));
+            $ticket->setLastName('Arnoult');
+            $ticket->setFirstName('Marc');
+            $ticket->setCountry('France');
+            $ticket->setBirthday(new \DateTime('1991-09-01'));
+            $ticket->setType("Journée");
+            $ticket->setPrice(16);
+            $ticket->setCommand($command);
+            $command->setType($ticket->getType());
+            $manager->persist($ticket);
+        }
 
-        $manager->persist($ticket);
         $manager->flush();
     }
 }
