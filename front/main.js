d8558b90ece29d42466c845b9bf58f65e3ebc1d8
Imac-marc <marc.arnoult@hotmail.fr>
2017-08-27 21:41:02 +0200

    Resolve #9 adding google maps

--- front/main.js
+++ front/main.js
@@ -4,11 +4,24 @@ import Citation from './components/Citation.vue';
 import Reservation from './pages/Reservation.vue';
 import VueRouter from 'vue-router'
 import store from './store/ReservationStore';
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.use(VueRouter);
+Vue.use(VueGoogleMaps, {
+    load: {key: 'AIzaSyAUSnEiemV5tXF7Zz7ZgNYuPnIOKOe547c'}
+});
+
 Window.store = store;
 
 new Vue({
+    data() {
+        return {
+            center: {lat:48.860294, lng:2.338629},
+            markers: [{
+                position: {lat:48.860294, lng:2.338629}
+            }]
+        }
+    },
     el: '#app',
     components: {
         Citation,
