8681e9361fc3ca62b35ea853edd9f1bef7a9154b
Imac-marc <marc.arnoult@hotmail.fr>
2017-08-27 17:56:49 +0200

    correct path email

--- src/AppBundle/Domain/Service/TicketSender.php
+++ src/AppBundle/Domain/Service/TicketSender.php
@@ -39,7 +39,7 @@ class TicketSender
             ->setTo($data->getEmail())
             ->setBody(
                 $this->twig->render(
-                    'command.html.twig', [
+                    ':Emails:command.html.twig', [
                         'command' => $data,
                         'numCommand' => $numCommand
                     ]),
